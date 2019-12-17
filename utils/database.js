0984a3f545f48b1c9d66ff72e312576139896432
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2019-12-17 05:29:10 -0500

    fixing bugs

--- utils/database.js
+++ utils/database.js
@@ -1,18 +1,19 @@
 const Sequelize = require("sequelize");
 const enviroment = process.env.DEV_ENV;
 const connectionString =
-    enviroment === "production"
-        ? process.env.LOCAL_DATABASE_CONNECTION_STRING
-        : process.env.DATABASE_CONNECTION_STRING;
-const sequelize = new Sequelize(process.env.JAWSDB_URL, {
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    logging: false
+  enviroment === "production"
+    ? process.env.LOCAL_DATABASE_CONNECTION_STRING
+    : process.env.DATABASE_CONNECTION_STRING;
+// process.env.JAWSDB_URL
+const sequelize = new Sequelize("inote-database", "root", "", {
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  logging: false
 });
 
 module.exports = sequelize;
